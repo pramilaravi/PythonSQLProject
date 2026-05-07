86c802137eef4a8065f80265dd072af56eb3a448
pramilaravi <pramilaeceian@gmail.com>
2026-05-06 21:21:25 -0500

    updated the usernmae manikandan

--- db_testing_framework/conftest.py
+++ db_testing_framework/conftest.py
@@ -12,7 +12,7 @@ def db():
 def db_with_data(db):
     cursor = db.cursor()
     cursor.executemany("INSERT INTO USERS (name,email,active) VALUES (?,?,?)",[("pram","ap@gmail.com","1"),
-                                                                               ("mani","mani@gmail.com","1"),
+                                                                               ("manikandan","mani@gmail.com","1"),
                                                                                ("adi","adhi@gmail.com","0"),])
     db.commit()
     return db
