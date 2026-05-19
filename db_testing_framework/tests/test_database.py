ed77c75a41e9c3466ed69f4f3892f4a1bec63b3f
pramilaravi <pramilaeceian@gmail.com>
2026-05-19 12:10:13 -0500

    updated the usernmae manikandan

--- db_testing_framework/tests/test_database.py
+++ db_testing_framework/tests/test_database.py
@@ -1,9 +1,13 @@
 import sqlite3
 import sys
-
+import logging
 from db.connection import get_connection, create_tables
 
+logger = logging.getLogger(__name__)
+
 def test_user_table_exists():
+
+    logger.info("test_user_table_exists Test started")
     conn = get_connection()
     create_tables(conn)
     cursor =conn.cursor()
@@ -11,6 +15,7 @@ def test_user_table_exists():
     where type ='table' and name ='users'""")
     result = cursor.fetchone()
     assert result is not None,"users table doesn't exist"
+    logger.info("test_user_table_exists Test completed")
     conn.close()
 
 def test_insert_user():
